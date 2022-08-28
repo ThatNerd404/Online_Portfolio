19b227272de6586960d10b3d1fc7dd4120e6be28
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-08-28 18:51:24 -0500

    Test if flask works on Raspberry pi
    test template

--- bray_portfolio.py
+++ bray_portfolio.py
@@ -1,5 +1,11 @@
 from flask import Flask , redirect , url_for , render_template , request , session , flash
 
+app = Flask(__name__)
+def home():
+    return "Hello! this is the test page <h1>HELLO<h1>"
+
+if __name__ == "__main__":
+    app.run()
 
 
 
