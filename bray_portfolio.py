f4c07e2fe4e9e58603562438d710ebc9c07024c6
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-08-28 19:42:37 -0500

    Fixing bug

--- bray_portfolio.py
+++ bray_portfolio.py
@@ -1,11 +1,12 @@
 from flask import Flask , redirect , url_for , render_template , request , session , flash
 
 app = Flask(__name__)
+@app.route('/')
 def home():
-    return "Hello! this is the test page <h1>HELLO<h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
 
 
