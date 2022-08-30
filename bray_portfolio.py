f17d8da92348f22649d4fc82f1d1a98d9a6a6e69
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-08-29 19:12:00 -0500

    To view it with home

--- bray_portfolio.py
+++ bray_portfolio.py
@@ -1,6 +1,7 @@
 from flask import Flask , redirect , url_for , render_template , request , session , flash
 
 app = Flask(__name__)
+@app.route('/home')
 @app.route('/')
 def home():
     return render_template("index.html")
