fb70131ec87de96c01f4f792194113d167af1c42
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2023-06-10 09:27:23 -0500

    Change file name

--- bray_portfolio.py
+++ bray_portfolio.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("Home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
